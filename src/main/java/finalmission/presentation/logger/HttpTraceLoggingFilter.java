b1d9897972e6ef235fb5fa00479a0d9669fbdf02
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-03 17:08:11 +0900

    feat: 로깅

---
+++ src/main/java/finalmission/presentation/logger/HttpTraceLoggingFilter.java
@@ -0,0 +1,42 @@
+package finalmission.presentation.logger;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class HttpTraceLoggingFilter extends OncePerRequestFilter {
+
+    private static final String TRACE_ID = "trace_id";
+
+    @Override
+    protected void doFilterInternal(
+            final HttpServletRequest request,
+            final HttpServletResponse response,
+            final FilterChain chain
+    ) throws ServletException, IOException {
+        try {
+            MDC.put(TRACE_ID, generateTraceId());
+            chain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+
+    private String generateTraceId() {
+        return "http-" + UUID.randomUUID()
+                .toString()
+                .substring(0, 8);
+    }
+}
